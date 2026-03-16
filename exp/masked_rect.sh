2cd8b27d1b93d4cadb240a84ac9c158c1836660c
Hoju Shin <qut1526@gmail.com>
2026-03-16 17:12:09 +0900

    update

--- exp/masked_rect.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-CONFIG=./options/masked_rect.yml
-
-CUDA_VISIBLE_DEVICES=1 torchrun --nproc_per_node=1 --master_port=1204 basicsr/rect_train.py -opt $CONFIG --launcher pytorch
--- exp/vanilla_concat.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-CONFIG=./options/vanilla_concat.yml
-DEVICES=2,3
-
-CUDA_VISIBLE_DEVICES=$DEVICES torchrun --nproc_per_node=2 --master_port=4445 basicsr/rect_train.py -opt $CONFIG --launcher pytorch
